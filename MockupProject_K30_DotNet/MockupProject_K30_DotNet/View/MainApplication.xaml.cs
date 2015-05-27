87d5c76195923dc087cc6353fe172ceb3aa2b1ca
DatTP <trphatdat@gmail.com>
2015-05-27 16:52:40 +0700

    Revert "Update Tab"
    This reverts commit 5102234164ce2ecc7e14f340031181590e9d661a.

--- MockupProject_K30_DotNet/MockupProject_K30_DotNet/View/MainApplication.xaml.cs
+++ MockupProject_K30_DotNet/MockupProject_K30_DotNet/View/MainApplication.xaml.cs
@@ -19,30 +19,10 @@ namespace MockupProject_K30_DotNet.View
     /// </summary>
     public partial class MainApplication : Window
     {
-        private List<TabItem> _tabItems;
-        private TabItem _tabAdd;
-
         public MainApplication()
         {
             InitializeComponent();
             new LoadingWindow().ShowDialog();
-            // initialize tabItem array
-            _tabItems = new List<TabItem>();
-
-            // add a tabItem with + in header 
-            _tabAdd = new TabItem();
-            _tabAdd.Header = "+";
-            // tabAdd.MouseLeftButtonUp += new MouseButtonEventHandler(tabAdd_MouseLeftButtonUp);
-
-            _tabItems.Add(_tabAdd);
-
-            // add first tab
-            this.AddTabItem();
-
-            // bind tab control
-            tabDynamic.DataContext = _tabItems;
-
-            tabDynamic.SelectedIndex = 0;
         }
 
         private void Button_Close(object sender, RoutedEventArgs e)
@@ -82,122 +62,6 @@ namespace MockupProject_K30_DotNet.View
                 else
                 {
                     this.WindowState = WindowState.Normal;
-                }
-            }
-        }
-        private TabItem AddTabItem()
-        {
-            int count = _tabItems.Count;
-
-            // create new tab item
-            TabItem tab = new TabItem();
-
-            tab.Header = string.Format("Tab {0}", count);
-            tab.Name = string.Format("tab{0}", count);
-            tab.HeaderTemplate = tabDynamic.FindResource("TabHeader") as DataTemplate;
-
-            tab.MouseDoubleClick += new MouseButtonEventHandler(tab_MouseDoubleClick);
-
-            // add controls to tab item, this case I added just a textbox
-            TextBox txt = new TextBox();
-            txt.Name = "txt";
-
-            tab.Content = txt;
-
-            // insert tab item right before the last (+) tab item
-            _tabItems.Insert(count - 1, tab);
-
-            return tab;
-        }
-
-        private void tabAdd_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            // clear tab control binding
-            tabDynamic.DataContext = null;
-
-            TabItem tab = this.AddTabItem();
-
-            // bind tab control
-            tabDynamic.DataContext = _tabItems;
-
-            // select newly added tab item
-            tabDynamic.SelectedItem = tab;
-        }
-
-        private void tab_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            TabItem tab = sender as TabItem;
-
-            TabProperty dlg = new TabProperty();
-
-            // get existing header text
-            dlg.txtTitle.Text = tab.Header.ToString();
-
-            if (dlg.ShowDialog() == true)
-            {
-                // change header text
-                tab.Header = dlg.txtTitle.Text.Trim();
-            }
-        }
-
-        private void tabDynamic_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            TabItem tab = tabDynamic.SelectedItem as TabItem;
-            if (tab == null) return;
-
-            if (tab.Equals(_tabAdd))
-            {
-                // clear tab control binding
-                tabDynamic.DataContext = null;
-
-                TabItem newTab = this.AddTabItem();
-
-                // bind tab control
-                tabDynamic.DataContext = _tabItems;
-
-                // select newly added tab item
-                tabDynamic.SelectedItem = newTab;
-            }
-            else
-            {
-                // your code here...
-            }
-        }
-
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
-            string tabName = (sender as Button).CommandParameter.ToString();
-
-            var item = tabDynamic.Items.Cast<TabItem>().Where(i => i.Name.Equals(tabName)).SingleOrDefault();
-
-            TabItem tab = item as TabItem;
-
-            if (tab != null)
-            {
-                if (_tabItems.Count < 3)
-                {
-                    MessageBox.Show("Cannot remove last tab.");
-                }
-                else if (MessageBox.Show(string.Format("Are you sure you want to remove the tab '{0}'?", tab.Header.ToString()),
-                    "Remove Tab", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-                {
-                    // get selected tab
-                    TabItem selectedTab = tabDynamic.SelectedItem as TabItem;
-
-                    // clear tab control binding
-                    tabDynamic.DataContext = null;
-
-                    _tabItems.Remove(tab);
-
-                    // bind tab control
-                    tabDynamic.DataContext = _tabItems;
-
-                    // select previously selected tab. if that is removed then select first tab
-                    if (selectedTab == null || selectedTab.Equals(tab))
-                    {
-                        selectedTab = _tabItems[0];
-                    }
-                    tabDynamic.SelectedItem = selectedTab;
                 }
             }
         }
